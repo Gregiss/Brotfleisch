880e01bf57335649ad52f834bcaa6a6a5fd9ea61
Hurricane404 <46573030+Hurricane404@users.noreply.github.com>
2020-11-04 21:41:25 -0300

    Fix

--- server.js
+++ server.js
@@ -1,17 +1,9 @@
 const express = require('express')
 const path = require('path')
-// const sqlite = require('sqlite-sync')
-// const setup = require('./setup')
-const verPedidos = require('./verPedidos')
-const logar = require('./user/login.js')
 const cardapio = require('./cardapio.json')
 const adicional = require('./adicional.json')
 const token = "yE-~!i]a_2F]FHIF4$8WKW1Q9A)c;81Rtmyl5oT@LQPmJ#fb!yI-Z?^24bK.e~Rj1)NN}aZK<UP!cPE67&`7!<i|8;tu*V*VM.ud`.-g$%DW;ylQuFX~EE~3i,m~46CCDB3CE93BE7"
 const user = {user: 'brot', password: 'diegoBrot'}
-
-// sqlite.connect('./brot.db')
-//Setup do programa
-// setup()
 
 const app = express()
 const server = require('http').createServer(app)
