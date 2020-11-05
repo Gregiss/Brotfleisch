704cd9c11ee07526fd8247f424ceea0af4c821cd
Hurricane404 <46573030+Hurricane404@users.noreply.github.com>
2020-11-04 21:21:01 -0300

    Initial

---
+++ public/index.js
@@ -0,0 +1,128 @@
+const socket = io('http://localhost:8080');
+const sound = new Audio('./notifica.wav')
+
+const app = new Vue({
+    el: "#app",
+    data: {
+        login: '',
+        senha: '',
+        logged: false,
+        path: window.location.pathname,
+        cardapio: [],
+        pedidoCardapio: [],
+        total: 0,
+        adicional: [],
+        totalAdicional: [],
+        cliente: "",
+        pedidos: []
+    },
+    mounted(){
+        socket.emit('card')
+        const vue = this
+        socket.on('cardapio', data => {
+            vue.cardapio = data.cardapio
+            vue.pedidoCardapio = [...data.cardapio],
+            vue.adicional = data.adicional
+            for(let i = 0; i < vue.pedidoCardapio.length; i++){
+                vue.pedidoCardapio[i].adicional = []
+            }
+            vue.totalAdicional = []
+        })
+        socket.emit('listarPedidos')
+        socket.on('novoPedido', data => {
+            if(vue.path.includes("/pedidos")){
+                sound.play()
+                vue.pedidos = data
+                vue.pedidos = vue.pedidos.reverse()
+            } 
+        })
+        if(window.localStorage.getItem('token')){
+            socket.emit('auth', window.localStorage.getItem('token'))
+        }
+        socket.on('token', token => {
+            window.localStorage.setItem('token', token)
+        })
+        socket.on('authed', () => {
+            vue.logged = true
+        })
+    },
+    methods: {
+        entrar(e){
+            socket.emit('login', {user: this.login, password: this.senha})
+            e.preventDefault()
+        },
+        updatePreco(){
+            var total = 0;
+            for(let i = 0; i < this.pedidoCardapio.length; i++){
+                if(this.pedidoCardapio[i].quantidade > 0){
+                    total += this.pedidoCardapio[i].quantidade * this.pedidoCardapio[i].preco
+                    for(let i = 0; i < this.totalAdicional.length; i++){
+                        const found = this.totalAdicional[i]
+                        if(found){
+                            total += parseFloat(this.totalAdicional[i].preco) * parseFloat(this.totalAdicional[i].qt)
+                        }
+                    }
+                }
+            }
+            this.total = total
+        },
+        novoPedido(){
+            const vue = this
+            if(this.cliente.trim().length == 0){
+                Toastify({
+                    text: "Preencha o nome do cliente",
+                    backgroundColor: "linear-gradient(to right, rgba(227,11,31,1),  rgba(203,20,37,0.9304096638655462))",
+                    className: "info",
+                }).showToast();
+            } else{
+            socket.emit("novoPedidoAa", {
+                pedidos: vue.pedidoCardapio,
+                adicional: vue.totalAdicional,
+                total: vue.total,
+                cliente: vue.cliente,
+                token: window.localStorage.getItem('token')
+            })
+            Toastify({
+                text: "Pedido enviado",
+                backgroundColor: "linear-gradient(to right, rgba(227,11,31,1),  rgba(203,20,37,0.9304096638655462))",
+                className: "info",
+            }).showToast();
+            socket.emit('card')
+            this.cliemte = ""
+        }
+        },
+        addAdicional(ad, id, lanche){  
+            this.pedidoCardapio[id].adicional.push(ad)
+            const found = this.totalAdicional.find(e => e.Lanche == lanche && e.adicional == ad.name)
+            if(found){
+                const id = this.totalAdicional.indexOf(found)
+                this.totalAdicional[id].qt++
+            } else{
+                this.totalAdicional.push({
+                    Lanche: lanche,
+                    adicional: ad.name,
+                    qt: 1,
+                    preco: ad.preco
+                })
+            }
+            this.updatePreco()
+        },
+        removeAdicional(ad, id, lanche){
+            const found = this.totalAdicional.find(e => e.Lanche == lanche && e.adicional == ad.name)
+            const found2 = this.pedidoCardapio[id].adicional.find(e => e.Lanche == lanche && adicional == ad.name)
+            if(found2){
+                const id2 = this.pedidoCardapio[id].adicional[found2]
+                this.pedidoCardapio[id].addAdicional.splice(id2, 1)
+            }
+            if(found){
+                const id3 = this.totalAdicional.indexOf(found)
+                if(this.totalAdicional[id3].qt > 0){
+                    --this.totalAdicional[id3].qt
+                } else{
+                    this.pedidoCardapio[id].addAdicional.splice(id2, 1)
+                }
+            }
+            this.updatePreco()
+        }
+    }
+})
---
+++ server.js
@@ -0,0 +1,67 @@
+const express = require('express')
+const path = require('path')
+// const sqlite = require('sqlite-sync')
+// const setup = require('./setup')
+const verPedidos = require('./verPedidos')
+const logar = require('./user/login.js')
+const cardapio = require('./cardapio.json')
+const adicional = require('./adicional.json')
+const token = "yE-~!i]a_2F]FHIF4$8WKW1Q9A)c;81Rtmyl5oT@LQPmJ#fb!yI-Z?^24bK.e~Rj1)NN}aZK<UP!cPE67&`7!<i|8;tu*V*VM.ud`.-g$%DW;ylQuFX~EE~3i,m~46CCDB3CE93BE7"
+const user = {user: 'brot', password: 'diegoBrot'}
+
+// sqlite.connect('./brot.db')
+//Setup do programa
+// setup()
+
+const app = express()
+const server = require('http').createServer(app)
+const io = require('socket.io')(server)
+
+app.use(express.static(path.join(__dirname, 'public')))
+const porta = process.env.PORT || 8080
+
+app.set('views', path.join(__dirname, 'public'))
+app.engine('html', require('ejs').renderFile)
+
+app.use('/', (req, res) => {
+	res.render('index.html')
+})
+
+app.use('/pedidos', (req, res) => {
+	res.render('pedidos.html')
+})
+
+var pedidos = []
+
+io.on('connection', socket => {
+	socket.on('login', data => {
+		if(data.user && data.password){
+			if(user.user.toLowerCase() == data.user.toLowerCase() && user.password.toLowerCase() == data.password.toLowerCase()){
+				socket.emit('token', token)
+			}
+		}
+	})
+	socket.on('card', data => {
+		socket.emit('cardapio', {cardapio: cardapio, adicional: adicional})
+		socket.join('brot')
+	})
+	socket.on('novoPedidoAa', data => {
+		if(data.token == token){
+			data.id = pedidos.length + 1
+			pedidos.push(data)
+			io.sockets.in(`brot`).emit('novoPedido', pedidos)
+		}
+	})
+	socket.on('listarPedidos', data => {
+		io.sockets.in(`brot`).emit('novoPedido', pedidos)
+	})
+	socket.on('auth', data => {
+		if(data == token){
+			socket.emit('authed', true)
+		}
+	})
+})
+
+server.listen(porta, () => {
+	console.log("starting")
+})
